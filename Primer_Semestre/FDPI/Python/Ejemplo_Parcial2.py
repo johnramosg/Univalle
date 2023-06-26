184f698b8cb0e11e5934148b22295cff3418a067
John Ramoss <ramos.john@correounivalle.edu.co>
2023-06-26 00:25:44 -0500

    Nuevos Archivos, Terminar el Ejemplo 2

---
+++ Primer_Semestre/FDPI/Python/Ejemplo_Parcial2.py
@@ -0,0 +1,56 @@
+from tkinter import *
+
+
+def calcular():
+    TL = int(ELibros.get()) * 50000
+    TR = int(ERevistas.get()) * 15000
+    total = TL + TR
+    EVenta.delete(0, END)
+    EVenta.insert(0, total)
+    ELibros.delete(0, END)
+    ERevistas.delete(0, END)
+
+
+def totales():
+    pass
+
+
+gui = Tk()
+gui.geometry("")
+
+
+marco1 = Frame(gui)
+LLibros = Label(marco1, text="Libros")
+LRevistas = Label(marco1, text="Revistas")
+ELibros = Entry(marco1, width=10)
+ERevistas = Entry(marco1, width=10)
+LLibros.grid(row=0, column=0)
+ELibros.grid(row=0, column=1)
+LRevistas.grid(row=1, column=0)
+ERevistas.grid(row=1, column=1)
+
+marco2 = Frame(gui)
+LVenta = Label(marco2, text="Valor a pagar en la venta actual")
+EVenta = Entry(marco2, width=15)
+LVenta.grid(row=0, column=0)
+EVenta.grid(row=0, column=1)
+
+marco3 = Frame(gui)
+LTotalLibros = Label(marco3, text="Total libros vendidos")
+ETotalLibros = Entry(marco3, width=15)
+LTotalRevistas = Label(marco3, text="Total revistas vendidas")
+ETotalRevistas = Entry(marco3, width=15)
+LTotalLibros.grid(row=0, column=0)
+ETotalLibros.grid(row=0, column=1)
+LTotalRevistas.grid(row=1, column=0)
+ETotalRevistas.grid(row=1, column=1)
+BCalcular = Button(gui, text="Calcular valor venta", command=calcular)
+BTotales = Button(gui, text="Calcular totales", command=totales)
+
+marco1.pack()
+BCalcular.pack()
+marco2.pack()
+BTotales.pack()
+marco3.pack()
+
+gui.mainloop()
