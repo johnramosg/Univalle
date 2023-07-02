56f8070ab89d456407a166e48c2a8ff7b1fd853d
John Ramoss <ramos.john@correounivalle.edu.co>
2023-07-01 20:57:13 -0500

    Cambios

--- Cursos/Pygame/Lineas_Figuras.py
+++ Cursos/Pygame/Lineas_Figuras.py
@@ -5,8 +5,8 @@ from pygame.locals import *
 pygame.init()
 ventana = pygame.display.set_mode(
     (
-        400,
-        300,
+        1920,
+        1080,
     )
 )
 pygame.display.set_caption("Titulo de la ventana")
