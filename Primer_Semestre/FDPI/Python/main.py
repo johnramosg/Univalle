ed2606e93b180140355912f94b3e1da96e4698f0
John Ramos <ramos.john@correounivalle.edu.co>
2023-07-17 22:13:39 -0500

    Nuevo archivo

---
+++ Primer_Semestre/FDPI/Python/main.py
@@ -0,0 +1,34 @@
+from pygame.locals import *
+import pygame,sys
+
+def posicion():
+    print(pygame.mouse.get_pos())
+
+
+
+pygame.init()
+win=pygame.display.set_mode((300,300))
+pygame.display.set_caption("Hola")
+fuente=pygame.font.SysFont("italic",70)
+texto=fuente.render("L",True,(255,0,0))
+
+
+pygame.draw.circle(win,(255,0,0),(110,100),50)
+pygame.draw.circle(win,(255,0,0),(190,100),50)
+pygame.draw.circle(win,(0,0,0),(110,100),45)
+pygame.draw.circle(win,(0,0,0),(190,100),45)
+pygame.draw.line(win,(255,0,0),(68,125),(145,250),5)
+pygame.draw.line(win,(255,0,0),(230,125),(145,250),5)
+pygame.draw.line(win,(0,0,0),(205, 137),(95, 140),37)
+pygame.draw.line(win,(0,0,0),(216, 129),(196, 157),16)
+pygame.draw.line(win,(0,0,0),(79, 125),(103, 168),14)
+win.blit(texto, [140, 120])
+
+while True:
+    for evento in pygame.event.get():
+        if evento.type==QUIT:
+            pygame.quit()
+            sys.exit()
+        if evento.type==MOUSEBUTTONUP:
+            posicion()
+    pygame.display.update()
