e59c2faf3b9e92c0edae0bdf43b3027947c40a35
John Ramos <ramos.john@correounivalle.edu.co>
2023-07-30 22:08:28 -0500

    Nuevos Archivos de C++

---
+++ Cursos/C++/HolaMund.cpp
@@ -0,0 +1,11 @@
+//Primer programa en C++
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  
+  cout<<"Hola Mundo :D"<<endl;
+
+  return 0; 
+}
---
+++ Cursos/C++/TDatos.cpp
@@ -0,0 +1,17 @@
+//Tipos de datos
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  int numero=15;
+  float numerofloat=1.8;
+  double mayor=16.3456;
+  char letra='J';
+  cout<<numero<<endl;
+  cout<<numerofloat<<endl;
+  cout<<mayor<<endl;
+  cout<<letra<<endl;
+  return 0;
+}
